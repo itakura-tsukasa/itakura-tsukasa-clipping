55e873c1c2472bf3d35744a036be6626716c2bed
K.S <none>
2024-03-21 13:57:43 +0900

    フォーム外見設計

--- clipping/clipping/home.Designer.cs
+++ clipping/clipping/home.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace clipping
 {
-    partial class Form1
+    partial class home
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,15 +39,38 @@
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.Size = new System.Drawing.Size(54, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "clippingApp";
+            this.label1.Text = "home";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 60F);
+            this.button1.Location = new System.Drawing.Point(16, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(344, 188);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 60F);
+            this.button2.Location = new System.Drawing.Point(444, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(344, 188);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "閲覧";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "clipping";
@@ -57,6 +82,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- clipping/clipping/view.cs
+++ clipping/clipping/view.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace clipping
 {
-    public partial class Form1 : Form
+    public partial class view : Form
     {
-        public Form1()
+        public view()
         {
             InitializeComponent();
         }
