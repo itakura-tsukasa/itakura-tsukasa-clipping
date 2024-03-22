3f89cc907bb9b0916a93e6b0b0521c2038cc8358
K.S <none>
2024-03-22 10:24:33 +0900

    XMLで保存機能を付けた 保存先のファイルを今は直接Debugに入れているので ビルドの時には注意が必要

--- clipping/clipping/store.cs
+++ clipping/clipping/store.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Linq;
+using System.Xml.Linq;
+
 
 namespace clipping
 {
@@ -25,13 +28,44 @@ namespace clipping
 
         private void button2_Click(object sender, EventArgs e)
         {
+
             if(textBox1.TextLength == 0)
             {
                 MessageBox.Show("タイトルを入力してください",
-                    "エラー",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
+                    "エラー",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
             }
+            if (textBox2.TextLength == 0)
+            {
+                MessageBox.Show("本文を入力してください",
+                    "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox3.TextLength == 0)
+            {
+                MessageBox.Show("情報ソースを入力してください",
+                    "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            XMLWriteArticle(textBox1.Text, textBox2.Text, textBox3.Text);
+
+        }
+
+        private void XMLWriteArticle (string title,string body,string source)
+        {
+            
+            XElement xml = XElement.Load(@"./clip_article.xml");
+
+            XElement data = new XElement("article",
+                new XElement("title", title),
+                new XElement("body", body),
+                new XElement("source", source));
+
+            xml.Add(data);
+
+            xml.Save(@"./clip_article.xml");
         }
     }
 }
